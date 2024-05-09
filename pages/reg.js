36f5833e2179321f2d2d7c2b0e2034aa039c4a68
Serhiy Vahnyuk <115102489+SerhiyVahnyuk@users.noreply.github.com>
2024-05-09 17:23:31 +0300

    Update reg.js

--- pages/reg.js
+++ pages/reg.js
@@ -28,7 +28,7 @@ export default function Reg() {
   const RegisterButton = async () => {
     console.log(formData)
     if (formData.password == formData.confirmPassword){
-      const response = await fetch("http://localhost:3000/user", {
+      const response = await fetch("https://markerplace.onrender.com/user", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -101,7 +101,7 @@ export default function Reg() {
         </button>
         <div className="redirectButton">
             <span>Вже є акаунт?</span>
-            <a href="http://localhost:3001/auth">Увійди</a>
+            <a href="/auth">Увійди</a>
         </div>
     </form>
     
